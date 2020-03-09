b4ee7a42130c743674bec5e1a3218619b152f730
Uday Akula <udaykiran199715@gmail.com>
2020-03-09 23:21:30 +0530

    Bill Generator

--- masai/sprint-2/script.js
+++ masai/sprint-2/script.js
@@ -21,36 +21,6 @@ function idly() {
     var bill = document.getElementById("bill")
     bill.setAttribute('class', 'add')
 
-    //print the headings
-
-    // var row1 = document.createElement('tr')
-    // var col11 = document.createElement('td')
-    // col11.textContent = "Item"
-    // col11.style.color = "tomato"
-    // col11.setAttribute('class','col')
-
-    // var col12 = document.createElement('td')
-    // col12.textContent = "QTY"
-    // col12.style.color = "tomato"
-    // col12.setAttribute('class','col')
-
-    // var col13 = document.createElement('td')
-    // col13.textContent = "Cost/plate"
-    // col13.style.color = "tomato"
-    // col13.setAttribute('class','col')
-
-    // var col14 = document.createElement('td')
-    // col14.textContent = "Price"
-    // col14.style.color = "tomato"
-    // col14.setAttribute('class','col')
-
-    // row1.append(col11)
-    // row1.append(col12)
-    // row1.append(col13)
-    // row1.append(col14)
-
-    // bill.append(row1)
-
 
     //print idly
 
@@ -85,26 +55,26 @@ function idly() {
 function dosa() {
 
     var row3 = document.createElement('tr')
-    var col31 = document.createElement('td')
-    col31.textContent = "Dosa"
-    col31.setAttribute('class', 'col')
+    var col3_1 = document.createElement('td')
+    col3_1.textContent = "Dosa"
+    col3_1.setAttribute('class', 'col')
 
-    var col32 = document.createElement('td')
-    col32.textContent = dosaQuantity.value
-    col32.setAttribute('class', 'col')
+    var col3_2 = document.createElement('td')
+    col3_2.textContent = dosaQuantity.value
+    col3_2.setAttribute('class', 'col')
 
-    var col33 = document.createElement('td')
-    col33.textContent = dosaPrice.value
-    col33.setAttribute('class', 'col')
+    var col3_3 = document.createElement('td')
+    col3_3.textContent = dosaPrice.value
+    col3_3.setAttribute('class', 'col')
 
-    var col34 = document.createElement('td')
-    col34.textContent = (dosaQuantity.value * dosaPrice.value)
-    col34.setAttribute('class', 'col')
+    var col3_4 = document.createElement('td')
+    col3_4.textContent = (dosaQuantity.value * dosaPrice.value)
+    col3_4.setAttribute('class', 'col')
 
-    row3.append(col31)
-    row3.append(col32)
-    row3.append(col33)
-    row3.append(col34)
+    row3.append(col3_1)
+    row3.append(col3_2)
+    row3.append(col3_3)
+    row3.append(col3_4)
 
     bill.append(row3)
 
@@ -114,26 +84,26 @@ function dosa() {
 function chapathi() {
 
     var row4 = document.createElement('tr')
-    var col41 = document.createElement('td')
-    col41.textContent = "Chapathi"
-    col41.setAttribute('class', 'col')
+    var col4_1 = document.createElement('td')
+    col4_1.textContent = "Chapathi"
+    col4_1.setAttribute('class', 'col')
 
-    var col42 = document.createElement('td')
-    col42.textContent = chapathiQuantity.value
-    col42.setAttribute('class', 'col')
+    var col4_2 = document.createElement('td')
+    col4_2.textContent = chapathiQuantity.value
+    col4_2.setAttribute('class', 'col')
 
-    var col43 = document.createElement('td')
-    col43.textContent = chapathiPrice.value
-    col43.setAttribute('class', 'col')
+    var col4_3 = document.createElement('td')
+    col4_3.textContent = chapathiPrice.value
+    col4_3.setAttribute('class', 'col')
 
-    var col44 = document.createElement('td')
-    col44.textContent = (chapathiQuantity.value * chapathiPrice.value)
-    col44.setAttribute('class', 'col')
+    var col4_4 = document.createElement('td')
+    col4_4.textContent = (chapathiQuantity.value * chapathiPrice.value)
+    col4_4.setAttribute('class', 'col')
 
-    row4.append(col41)
-    row4.append(col42)
-    row4.append(col43)
-    row4.append(col44)
+    row4.append(col4_1)
+    row4.append(col4_2)
+    row4.append(col4_3)
+    row4.append(col4_4)
 
     bill.append(row4)
 
@@ -143,26 +113,26 @@ function chapathi() {
 function puri() {
 
     var row5 = document.createElement('tr')
-    var col51 = document.createElement('td')
-    col51.textContent = "Puri"
-    col51.setAttribute('class', 'col')
+    var col5_1 = document.createElement('td')
+    col5_1.textContent = "Puri"
+    col5_1.setAttribute('class', 'col')
 
-    var col52 = document.createElement('td')
-    col52.textContent = puriQuantity.value
-    col52.setAttribute('class', 'col')
+    var col5_2 = document.createElement('td')
+    col5_2.textContent = puriQuantity.value
+    col5_2.setAttribute('class', 'col')
 
-    var col53 = document.createElement('td')
-    col53.textContent = puriPrice.value
-    col53.setAttribute('class', 'col')
+    var col5_3 = document.createElement('td')
+    col5_3.textContent = puriPrice.value
+    col5_3.setAttribute('class', 'col')
 
-    var col54 = document.createElement('td')
-    col54.textContent = (puriQuantity.value * puriPrice.value)
-    col54.setAttribute('class', 'col')
+    var col5_4 = document.createElement('td')
+    col5_4.textContent = (puriQuantity.value * puriPrice.value)
+    col5_4.setAttribute('class', 'col')
 
-    row5.append(col51)
-    row5.append(col52)
-    row5.append(col53)
-    row5.append(col54)
+    row5.append(col5_1)
+    row5.append(col5_2)
+    row5.append(col5_3)
+    row5.append(col5_4)
 
     bill.append(row5)
 
@@ -171,26 +141,26 @@ function puri() {
 function noodles() {
 
     var row6 = document.createElement('tr')
-    var col61 = document.createElement('td')
-    col61.textContent = "Noodles"
-    col61.setAttribute('class', 'col')
+    var col6_1 = document.createElement('td')
+    col6_1.textContent = "Noodles"
+    col6_1.setAttribute('class', 'col')
 
-    var col62 = document.createElement('td')
-    col62.textContent = noodlesQuantity.value
-    col62.setAttribute('class', 'col')
+    var col6_2 = document.createElement('td')
+    col6_2.textContent = noodlesQuantity.value
+    col6_2.setAttribute('class', 'col')
 
-    var col63 = document.createElement('td')
-    col63.textContent = noodlesPrice.value
-    col63.setAttribute('class', 'col')
+    var col6_3 = document.createElement('td')
+    col6_3.textContent = noodlesPrice.value
+    col6_3.setAttribute('class', 'col')
 
-    var col64 = document.createElement('td')
-    col64.textContent = (noodlesQuantity.value * noodlesPrice.value)
-    col64.setAttribute('class', 'col')
+    var col6_4 = document.createElement('td')
+    col6_4.textContent = (noodlesQuantity.value * noodlesPrice.value)
+    col6_4.setAttribute('class', 'col')
 
-    row6.append(col61)
-    row6.append(col62)
-    row6.append(col63)
-    row6.append(col64)
+    row6.append(col6_1)
+    row6.append(col6_2)
+    row6.append(col6_3)
+    row6.append(col6_4)
 
     bill.append(row6)
 
@@ -199,26 +169,26 @@ function noodles() {
 function paratha() {
 
     var row7 = document.createElement('tr')
-    var col71 = document.createElement('td')
-    col71.textContent = "Paratha"
-    col71.setAttribute('class', 'col')
+    var col7_1 = document.createElement('td')
+    col7_1.textContent = "Paratha"
+    col7_1.setAttribute('class', 'col')
 
-    var col72 = document.createElement('td')
-    col72.textContent = parathaQuantity.value
-    col72.setAttribute('class', 'col')
+    var col7_2 = document.createElement('td')
+    col7_2.textContent = parathaQuantity.value
+    col7_2.setAttribute('class', 'col')
 
-    var col73 = document.createElement('td')
-    col73.textContent = parathaPrice.value
-    col73.setAttribute('class', 'col')
+    var col7_3 = document.createElement('td')
+    col7_3.textContent = parathaPrice.value
+    col7_3.setAttribute('class', 'col')
 
-    var col74 = document.createElement('td')
-    col74.textContent = (parathaQuantity.value * parathaPrice.value)
-    col74.setAttribute('class', 'col')
+    var col7_4 = document.createElement('td')
+    col7_4.textContent = (parathaQuantity.value * parathaPrice.value)
+    col7_4.setAttribute('class', 'col')
 
-    row7.append(col71)
-    row7.append(col72)
-    row7.append(col73)
-    row7.append(col74)
+    row7.append(col7_1)
+    row7.append(col7_2)
+    row7.append(col7_3)
+    row7.append(col7_4)
 
     bill.append(row7)
 
@@ -246,8 +216,9 @@ function total() {
     // var sel = document.getElementById("sel").value
     // console.log(sel)
 
-    var gst = sum + (sum * 5 / 100)
+    var gst = sum * 5 / 100
     console.log(gst)
+    var totalGst = sum + gst
 
     var bill = document.getElementById("bill")
     bill.setAttribute('class', 'add')
@@ -263,12 +234,13 @@ function total() {
 
     var row9 = document.createElement('tr')
     var col9_1 = document.createElement('td')
-    //col9_1.textContent = "DISCOUNT"
+    col9_1.textContent = "DISCOUNT"
+    col9_1.colSpan = 2
     col9_1.setAttribute('class', 'col')
 
     var col9_2 = document.createElement('td')
-    //col9_2.textContent = "10% with No gst"
-    col9_2.colSpan = 3
+    col9_2.textContent = "10%"
+    col9_2.colSpan = 2
     col9_2.setAttribute('class', 'col')
 
     var row10 = document.createElement('tr')
@@ -296,8 +268,9 @@ function total() {
     var sel = document.getElementById("sel").value
     console.log(sel)
 
-    var discount = sum - (sum * 10 / 100)
+    var discount = sum * 10 / 100
     console.log(discount)
+    var sumDis = sum - discount + gst
 
     if (sum < 100) {
         row8.remove()   
@@ -308,23 +281,38 @@ function total() {
     } else if (sum > 100 & sel == 'SELECT') {
         col9_1.remove()
         col9_2.remove()
-        col10_2.textContent = gst
+        col10_2.textContent = totalGst
     } else if (sel == 'WEDNESDAY HUNGAMA' & sum > 100) {
-        row8.remove()
-        col9_1.textContent = "DISCOUNT"
-        col9_2.textContent = "10% with No GST"
-        col10_2.textContent = discount
+        col10_2.textContent = sumDis
     }
     else if (sel == 'FRIDAY FUN' & sum > 100) {
-        row8.remove()
-        col9_1.textContent = "DISCOUNT"
-        col9_2.textContent = "10% with No GST"
-        col10_2.textContent = discount
+        col10_2.textContent = sumDis
     } else if (sel == 'SUNDAY DHAMAKA' & sum > 100) {
-        row8.remove()
-        col9_1.textContent = "DISCOUNT"
-        col9_2.textContent = "10% with No GST"
-        col10_2.textContent = discount
+        col10_2.textContent = sumDis
     }
 
+
+    var row11 = document.createElement('tr')
+    var col11_1 = document.createElement('td')
+    var img1 = document.createElement('img')
+    img1.src="https://t4.ftcdn.net/jpg/02/43/55/79/240_F_243557949_n5gXht1IpYcgzRMFa9bd5KH7mG4rsWYV.jpg"
+    col11_1 = img1
+    //col10_1.setAttribute('class', 'col')
+
+    var col11_2 = document.createElement('td')
+    //var img = document.createElement('img')
+    col11_2.textContent = "Thank you Visit Again"
+    col11_2.colSpan = 2
+    col11_2.setAttribute('class', 'thank')
+    
+    var col11_4 = document.createElement('td')
+    var img2 = document.createElement('img')
+    img2.src="https://t4.ftcdn.net/jpg/02/43/55/79/240_F_243557949_n5gXht1IpYcgzRMFa9bd5KH7mG4rsWYV.jpg"
+    col11_4 = img2
+    //col10_1.setAttribute('class', 'col')
+
+    row11.append(col11_1)
+    row11.append(col11_2)
+    row11.append(col11_4)
+    bill.append(row11)
 }
